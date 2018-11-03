b34efb50dab4153f532eea05b61775168aacf546
Pierre-Dominique Putallaz <pierre-dominique.putallaz@nestle.com>
2018-11-03 16:13:32 +0100

    (perf) remove unused css

--- src/entry.js
+++ src/entry.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 
 // CSS dependencies
 import './index.scss';
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/components/header.min.css';
+import 'semantic-ui-css/components/menu.min.css';
+import 'semantic-ui-css/components/form.min.css';
+import 'semantic-ui-css/components/button.min.css';
 
 import App from './App';
 

967e4f0a549dbcca2f460a9263bd283e4b9f9ac3
Pierre-Dominique Putallaz <pierre-dominique.putallaz@nestle.com>
2018-11-11 12:48:29 +0100

    (polyfill) better polyfill

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// IE sucks...
+import 'es6-promise/auto';
 
 import App from './App';
 
-const AppWithStore = () => <App />;
+function renderApp() {
+  const AppWithStore = () => <App />;
 
-ReactDOM.render(<AppWithStore />, document.getElementById('root'));
+  ReactDOM.render(<AppWithStore />, document.getElementById('root'));
+}
+
+// Simple way to detect whether the browser needs polyfill.
+if (typeof window.fetch === 'function') {
+  renderApp();
+} else {
+  import(/* webpackChunkName: 'polyfill' */ './polyfill').then(() => {
+    renderApp();
+  });
+}

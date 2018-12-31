29909279325e18dddc562beb1b987b8501ab616e
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2018-12-31 11:48:25 +0100

    (home) fix test when running with the registation link activated

--- src/home-page/HomePage.spec.tsx
+++ src/home-page/HomePage.spec.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-testing-library';
 import HomePage from './HomePage';
 
+const HomePageWithRouter = () => (
+  <BrowserRouter>
+    <HomePage />
+  </BrowserRouter>
+);
+
 describe('HomePage', () => {
   it('should match the snapshot', () => {
-    const wrapper = render(<HomePage />);
+    const wrapper = render(<HomePageWithRouter />);
     expect(wrapper.baseElement).toMatchSnapshot();
   });
 
   it('should set the page title', () => {
     // Will be set by side effect through a hook
-    render(<HomePage />);
+    render(<HomePageWithRouter />);
     expect(document.title).toEqual('home.page.title');
   });
 });
